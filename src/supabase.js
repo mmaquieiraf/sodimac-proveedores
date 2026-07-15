e67296782f2521fcc2ce77d181d793aa140b5151
mmaquieiraf <matiasignaciof01@gmail.com>
2026-07-15 02:30:45 -0400

    1

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://zpxptembhqlmpkbctvml.supabase.co';
+const supabaseKey = 'sb_publishable_KGTeAmtcEyxPx1CMN_vmXw_akhi8aH5';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
