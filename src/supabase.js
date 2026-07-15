57778decb95fc8c4da91bd518f40f77a2f63d499
mmaquieiraf <matiasignaciof01@gmail.com>
2026-07-15 01:04:10 -0400

    vite1

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-// 1. URL base limpia (cortada justo antes del /rest)
-const supabaseUrl = 'https://zpxptembhqlmpkbctvml.supabase.co' 
+// 1. Vite lee la URL desde tu archivo .env
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
-// 2. Tu nueva API Key
-const supabaseKey = 'sb_publishable_KGTeAmtcEyxPx1CMN_vmXw_akhi8aH5' 
+// 2. Vite lee la API Key desde tu archivo .env
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
