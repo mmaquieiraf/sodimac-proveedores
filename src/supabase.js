98b302483bd2a8c5896c2f47f15f6e1e0b4a72a8
mmaquieiraf <matiasignaciof01@gmail.com>
2026-07-15 02:25:09 -0400

    1

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// 1. Vite lee la URL desde tu archivo .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-
-// 2. Vite lee la API Key desde tu archivo .env
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
