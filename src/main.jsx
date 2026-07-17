e72a1f0c45581083a629b63f8caffe1fae395509
mmaquieiraf <matiasignaciof01@gmail.com>
2026-07-17 15:35:38 -0400

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,3 @@
-// Añadir esto al principio del archivo
-if (process.env.NODE_ENV === 'production') {
-  console.log = () => {};
-  console.debug = () => {};
-  console.info = () => {};
-  console.warn = () => {};
-  console.error = () => {}; 
-}
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
