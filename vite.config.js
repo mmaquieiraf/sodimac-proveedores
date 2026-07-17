0d723ce86adeeba28ecb7d346ec24fc2052a519c
mmaquieiraf <matiasignaciof01@gmail.com>
2026-07-17 15:27:26 -0400

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true, // Elimina todos los console.log, info, warn en producción
-        drop_debugger: true,
-      },
-    },
-  },
-});
+  plugins: [react()]
+})
